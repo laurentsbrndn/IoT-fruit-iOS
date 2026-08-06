cb5bb1e8d50fb83cd116ab4adddb232d106af8d4
laurentsbrndn <brandonvikario607@gmail.com>
2026-08-06 13:02:55 +0700

    Initial Commit

---
+++ BIKI LiveTrack/BIKI_LiveTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIKI_LiveTrackApp.swift
+//  BIKI LiveTrack
+//
+//  Created by Laurentius Brandon Vikario on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BIKI_LiveTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
